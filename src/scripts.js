3a899289237f897ed8590e7133c788ea13a3f439
guillermo.fisher <guillermoandraefisher@gmail.com>
2018-06-08 16:41:15 -0400

    Refactored a tad.

--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,7 @@ var MeetupEvent = function () {
 
     var title, description, dateTime, humanDateTime, location = "TBD";
 
-    var run = () => {
+    var init = () => {
         var container = $("#meetup-event");
         container.find("h1").html(title);
         container.find("p.meetup-description").html(description);
@@ -11,23 +11,24 @@ var MeetupEvent = function () {
         container.find("span.address").html(location);
     };
 
-    return {
-        setEvent: function(e) {
-            title = e.name;
-            description = e.description;
-            dateTime = new Date(e.time);
-            humanDateTime = dateTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-                + " on "
-                + [dateTime.getMonth()+1, dateTime.getDate(), dateTime.getFullYear()].join('/');
-            run();
-        }
+    var setEvent = (e) => {
+        title = e.name;
+        description = e.description;
+        dateTime = new Date(e.time);
+        humanDateTime = dateTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+            + " on "
+            + [dateTime.getMonth()+1, dateTime.getDate(), dateTime.getFullYear()].join('/');
+        init();
     };
+
+    return { setEvent: setEvent };
 }();
 
 $(function () {
     $.ajax({
         url: "https://api.757colorcoded.org/events",
         method: "GET",
+        cache: true,
         success: function (data) {
             MeetupEvent.setEvent(data.results[0]);
         },
