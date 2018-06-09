eba7883e0db4d2e48896978ffcefc0323921b8ea
guillermo.fisher <guillermoandraefisher@gmail.com>
2018-06-08 20:56:58 -0400

    General cleanup.

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,23 @@
 
 var MeetupEvent = function () {
-
-    var title, description, dateTime, humanDateTime, location = "TBD";
-
+    var url, title, description, dateTime, humanDateTime, location = "TBD";
     var init = () => {
         var container = $("#meetup-event");
         container.find("h1").html(title);
         container.find("p.meetup-description").html(description);
         container.find("time").html(humanDateTime);
+        container.find(".meetup-url").attr("href", url);
         container.find("span.address").html(location);
     };
-
     var setEvent = (e) => {
+        url = e.event_url;
         title = e.name;
         description = e.description;
         dateTime = new Date(e.time);
-        humanDateTime = dateTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-            + " on "
-            + [dateTime.getMonth()+1, dateTime.getDate(), dateTime.getFullYear()].join('/');
+        humanDateTime = dateTime.toLocaleString("en-US", { timeZoneName: "short" });
+        location = e.venue.name;
         init();
     };
-
     return { setEvent: setEvent };
 }();
 
@@ -28,13 +25,7 @@ $(function () {
     $.ajax({
         url: "https://api.757colorcoded.org/events",
         method: "GET",
-        cache: true,
-        success: function (data) {
-            MeetupEvent.setEvent(data.results[0]);
-        },
-        error: function (error) {
-            console.log("The requested failed!");
-            console.log(error);
-        }
+        success: function (data) { MeetupEvent.setEvent(data.results[0]); },
+        error: function (error) { console.log("The requested failed!"); }
     });
 });
